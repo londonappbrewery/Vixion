9104490ab334f71117a2fdae4c19845b3658c9f6
angelabauer <angela_yu@hotmail.co.uk>
2015-10-20 16:20:13 +0100

    Starter Project

---
+++ Vixion/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Vixion
+//
+//  Created by Angela Yu on 25/01/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Drop your IBOutlets below this line
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //Drop your buttons (IBActions) below this line
+    
+
+
+}
+
